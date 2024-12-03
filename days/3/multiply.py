95b275bfc4bbc4490790bdf8f3c20f06c261a6c7
Kyle Daling <kyle-daling@live.com>
2024-12-02 22:46:16 -0700

    2024 Day 3 Part 2

--- days/3/multiply.py
+++ days/3/multiply.py
@@ -3,6 +3,7 @@ import pathlib
 import re
 
 MUL_RE = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+ENABLE_MUL_RE = re.compile(r"(?:do\(\)|don't\(\))|mul\((\d{1,3}),(\d{1,3})\)")
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -26,6 +27,19 @@ def get_multiplication_groups(the_input: str) -> list[tuple[int, int]]:
     return [(int(x), int(y)) for x, y in MUL_RE.findall(the_input)]
 
 
+def get_mult_enable_groups(the_input: str) -> list[tuple[int, int]]:
+    commands = []
+    enabled = True
+    for match in ENABLE_MUL_RE.finditer(the_input):
+        if match.group(0) == "do()":
+            enabled = True
+        elif match.group(0) == "don't()":
+            enabled = False
+        if enabled and match.group(0).startswith("mul"):
+            commands.append((int(match.group(1)), int(match.group(2))))
+    return commands
+
+
 def main():
     parser = get_parser()
     args = parser.parse_args()
@@ -33,6 +47,9 @@ def main():
 
     groups = get_multiplication_groups(lines)
     print(f"Sum of multiplications: {sum(x * y for x,y in groups)}")
+
+    enabled_groups = get_mult_enable_groups(lines)
+    print(f"Sum of enabled multiplications: {sum(x * y for x,y in enabled_groups)}")
 
 
 if __name__ == "__main__":
--- days/3/test_multiply.py
+++ days/3/test_multiply.py
@@ -1,5 +1,6 @@
 import pytest
 
+from multiply import get_mult_enable_groups
 from multiply import get_multiplication_groups
 
 
@@ -12,3 +13,13 @@ from multiply import get_multiplication_groups
 )
 def test_get_multiplication_groups(input_text, result):
     assert get_multiplication_groups(input_text) == result
+
+
+@pytest.mark.parametrize(
+    "input_text,result",
+    [
+        ("do()mul(44,46)don't()mul(2,2)", [(44, 46)]),
+    ],
+)
+def test_get_mult_enable_groups(input_text, result):
+    assert get_mult_enable_groups(input_text) == result
