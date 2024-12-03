15e41d8d07a71eace25fdc296a7284ec3aa9a35a
Kyle Daling <kyle-daling@live.com>
2024-12-02 21:54:31 -0700

    2024 Day 2 Part 2

--- days/2/safety.py
+++ days/2/safety.py
@@ -18,6 +18,8 @@ def get_parser() -> argparse.ArgumentParser:
 
 
 def is_pair_safe(a: int, b: int, last_difference: int = None) -> tuple[bool, int]:
+    """Returns true if the current difference between a and b is the same sign as
+    the last difference and the differences is greater than 1 or less than 3."""
     difference = a - b
     return (
         abs(difference) >= 1
@@ -36,12 +38,32 @@ def is_safe_report(levels: Report) -> bool:
     return True
 
 
+def minimize_distance_to_next(levels: Report) -> int:
+    value, compare, next_compare = levels
+    return value if abs(value - next_compare) < abs(compare - next_compare) else compare
+
+
+def minimize_distance_from_previous(levels: Report) -> int:
+    value, compare, next_compare = levels
+    return compare if abs(compare - value) < abs(next_compare - value) else next_compare
+
+
+def is_safe_dampened(levels: Report) -> bool:
+    """If the report isn't safe, brute force removals to see if any are."""
+    if is_safe_report(levels):
+        return True
+    for i in range(len(levels)):
+        if is_safe_report(levels[0:i] + levels[i + 1 : len(levels)]):
+            return True
+    return False
+
+
 def load_file(file_path: pathlib.Path) -> list[Report]:
     values = []
     with open(file_path, "r") as input_file:
         for line in input_file:
             if strip_line := line.strip():
-                values.append(tuple(int(x) for x in strip_line.split()))
+                values.append(list(int(x) for x in strip_line.split()))
     return values
 
 
@@ -50,8 +72,14 @@ def main():
     args = parser.parse_args()
     values = load_file(args.filename)
 
+    for value in values:
+        if not is_safe_report(value):
+            print(f"{value} {is_safe_report(value)} {is_safe_dampened(value)}")
+
     count = sum(1 if is_safe_report(x) else 0 for x in values)
     print(f"{count} reports are safe")
+    count_dampened = sum(1 if is_safe_dampened(x) else 0 for x in values)
+    print(f"{count_dampened} reports are safe with dampening applied.")
 
 
 if __name__ == "__main__":
--- days/2/test_safety.py
+++ days/2/test_safety.py
@@ -1,16 +1,19 @@
 import pytest
 
 from safety import is_pair_safe
+from safety import is_safe_dampened
 from safety import is_safe_report
 from safety import load_file
+from safety import minimize_distance_from_previous
+from safety import minimize_distance_to_next
 
 TEST_LEVELS = [
-    (7, 6, 4, 2, 1),
-    (1, 2, 7, 8, 9),
-    (9, 7, 6, 2, 1),
-    (1, 3, 2, 4, 5),
-    (8, 6, 4, 4, 1),
-    (1, 3, 6, 7, 9),
+    [7, 6, 4, 2, 1],
+    [1, 2, 7, 8, 9],
+    [9, 7, 6, 2, 1],
+    [1, 3, 2, 4, 5],
+    [8, 6, 4, 4, 1],
+    [1, 3, 6, 7, 9],
 ]
 
 TEST_FILE_CONTENT = """7 6 4 2 1
@@ -33,17 +36,37 @@ def test_is_pair_safe(a, b, last_difference, result):
 
 @pytest.mark.parametrize(
     "the_list,result",
-    [
-        ((7, 6, 4, 2, 1), True),
-        ((1, 2, 7, 8, 9), False),
-        ((9, 7, 6, 2, 1), False),
-        ((1, 3, 2, 4, 5), False),
-        ((8, 6, 4, 4, 1), False),
-        ((1, 3, 6, 7, 9), True),
-    ],
+    zip(TEST_LEVELS, (True, False, False, False, False, True)),
 )
 def test_is_safe_report(the_list, result):
     assert is_safe_report(the_list) == result
+
+
+@pytest.mark.parametrize(
+    "the_list,result",
+    list(zip(TEST_LEVELS, (True, False, False, True, True, True)))
+    + [
+        ([1, 2, 2, 2], False),
+        ([1, 2, 2, 3, 4, 5], True),
+        ([7, 10, 8, 10, 11], True),
+        ([29, 28, 27, 25, 26, 25, 22, 20], True),
+        ([1, 1, 2, 3, 4, 5], True),
+    ],
+)
+def test_is_safe_dampened(the_list, result):
+    assert is_safe_dampened(the_list) == result
+
+
+@pytest.mark.parametrize(
+    "levels,value", [((1, 10, 11), 10), ((2, 11, 3), 2), ((10, 9, 1), 9)]
+)
+def test_minimize_distance_to_next(levels, value):
+    assert minimize_distance_to_next(levels) == value
+
+
+@pytest.mark.parametrize("levels,value", [((27, 25, 26), 26)])
+def test_minimize_distance_from_previous(levels, value):
+    assert minimize_distance_from_previous(levels) == value
 
 
 def test_load_file(tmp_path):
