0ea1950a787c4aba245dd8447070ea9af4586253
Kyle Daling <kyle-daling@live.com>
2024-12-13 14:28:15 -0700

    Broken 2024 Day 9 Part 2 solution
    It's best to work on programming tasks when you have had more than 3
    hours of sleep.

--- days/9/disk_fragmenter.py
+++ days/9/disk_fragmenter.py
@@ -1,7 +1,28 @@
 import argparse
 import pathlib
+from typing import Generator
 
 DiskBlocks = list[int | str]
+
+
+class FileSpan:
+    def __init__(self, span):
+        self.span = span
+
+    def __len__(self):
+        return self.span
+
+
+class File(FileSpan):
+    """A span of file blocks with content."""
+
+    def __init__(self, span, num):
+        self.number = num
+        super().__init__(span)
+
+
+class Empty(FileSpan):
+    """A span of non-allocated blocks"""
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -16,6 +37,7 @@ def get_parser() -> argparse.ArgumentParser:
 
 
 def compute_compacted_checksum(disk_map: str) -> int:
+    """1 iteration through disk_map to compute the checksum."""
     checksum = 0
     block_idx = 0
 
@@ -35,6 +57,7 @@ def compute_compacted_checksum(disk_map: str) -> int:
 
 
 def expand_blocks(disk_map: str) -> DiskBlocks:
+    """Expand the sparse disk representation to block representation"""
     blocks = []
     block_idx = 0
     for i, number in enumerate(disk_map):
@@ -46,6 +69,7 @@ def expand_blocks(disk_map: str) -> DiskBlocks:
 
 
 def compact_blocks(disk_blocks: DiskBlocks) -> DiskBlocks:
+    """Compact blocks, placing them in any available free space"""
     updated_blocks = disk_blocks.copy()
 
     defragment_idx = len(disk_blocks) - 1
@@ -62,7 +86,53 @@ def compact_blocks(disk_blocks: DiskBlocks) -> DiskBlocks:
     return updated_blocks
 
 
+def get_free_blocks(blocks: DiskBlocks) -> Generator[tuple[int, int], None, None]:
+    free_idx = 0
+    free_length = 0
+
+    while free_idx + free_length < len(blocks):
+        if blocks[free_idx + free_length] == ".":
+            free_length += 1
+        elif free_length == 0 and blocks[free_idx] != ".":
+            free_idx += 1
+        elif free_length > 0 and blocks[free_idx + free_length] != ".":
+            yield (free_idx, free_length)
+            free_idx += free_length
+            free_length = 0
+
+
+def compact_files(disk_blocks: DiskBlocks) -> DiskBlocks:
+    updated_blocks = disk_blocks.copy()
+
+    compact_idx = len(disk_blocks) - 1
+    while disk_blocks[compact_idx] == ".":
+        compact_idx -= 1
+
+    while compact_idx >= 0:
+        if updated_blocks[compact_idx] == ".":
+            compact_idx -= 1
+        else:
+            test_idx = compact_idx
+            while updated_blocks[test_idx] == updated_blocks[compact_idx]:
+                test_idx -= 1
+            for free_idx, free_length in get_free_blocks(updated_blocks):
+                if free_length >= compact_idx - test_idx and free_idx < test_idx:
+                    updated_blocks[free_idx : free_idx + compact_idx - test_idx] = (
+                        updated_blocks[test_idx + 1 : compact_idx + 1]
+                    )
+                    updated_blocks[test_idx + 1 : compact_idx + 1] = [
+                        "." for _ in range(test_idx, compact_idx)
+                    ]
+                    break
+            compact_idx = test_idx
+        if compact_idx % 100 == 0:
+            print(compact_idx)
+
+    return updated_blocks
+
+
 def compute_checksum(disk_blocks: DiskBlocks) -> int:
+    """Compute the checksum for a filesystem represented as blocks"""
     checksum = 0
     for i, block_value in enumerate(disk_blocks):
         if block_value != ".":
@@ -81,9 +151,13 @@ def main():
     puzzle_input = load_file(args.filename)
 
     blocks = expand_blocks(puzzle_input)
-    defragmented = compact_blocks(blocks)
-    checksum = compute_checksum(defragmented)
+    compacted = compact_blocks(blocks)
+    checksum = compute_checksum(compacted)
     print(f"Filesystem checksum: {checksum}")
+
+    compacted_files = compact_files(blocks)
+    checksum_files = compute_checksum(compacted_files)
+    print(f"Filesystem checksum with file compaction: {checksum_files}")
 
 
 if __name__ == "__main__":
--- days/9/test_disk_fragmenter.py
+++ days/9/test_disk_fragmenter.py
@@ -1,9 +1,12 @@
-import pytest
+from itertools import zip_longest
 
-from disk_fragmenter import expand_blocks
+import pytest
 from disk_fragmenter import compact_blocks
+from disk_fragmenter import compact_files
 from disk_fragmenter import compute_checksum
 from disk_fragmenter import DiskBlocks
+from disk_fragmenter import expand_blocks
+from disk_fragmenter import get_free_blocks
 
 
 def str_to_disk_blocks(input_str: str) -> DiskBlocks:
@@ -19,6 +22,35 @@ COMPACTED_BLOCKS = [
     str_to_disk_blocks("022111222......"),
     str_to_disk_blocks("0099811188827773336446555566.............."),
 ]
+COMPACTED_FILES = [
+    str_to_disk_blocks("0..111....22222"),
+    str_to_disk_blocks("00992111777.44.333....5555.6666.....8888.."),
+]
+
+
+@pytest.mark.parametrize(
+    "disk_blocks,result",
+    zip(
+        TEST_BLOCKS,
+        [
+            ((1, 2), (6, 4)),
+            ((2, 3), (8, 3), (12, 3), (18, 1), (21, 1), (26, 1), (31, 1), (35, 1)),
+        ],
+    ),
+)
+def test_get_free_blocks(disk_blocks, result):
+    values = get_free_blocks(disk_blocks)
+    for i in result:
+        assert next(values) == i
+    with pytest.raises(StopIteration):
+        next(values)
+
+
+@pytest.mark.parametrize(
+    "disk_blocks,result", zip(TEST_BLOCKS[1:], COMPACTED_FILES[1:])
+)
+def test_compact_files(disk_blocks: DiskBlocks, result: DiskBlocks):
+    assert compact_files(disk_blocks) == result
 
 
 @pytest.mark.parametrize("disk_blocks,result", zip(TEST_BLOCKS, COMPACTED_BLOCKS))
